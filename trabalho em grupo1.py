d5c27855e6da9cb743a2e97dded37e058b4b3457
FCoppolla <felipe.v.coppolla@gmail.com>
2024-10-07 18:58:12 -0300

    part 1 do trabalho
    fim da primeira parte

--- trabalho em grupo1.py
+++ trabalho em grupo1.py
@@ -1,18 +1,19 @@
 #faça um programa que se o usuario inserir 1 ele escolhe a classe barbaro, se 2 escolhe a classe mago, se 3 escolhe a classe arqueiro.
 
 
-classe = int(input("Qual será a sua classe escolhida 1,2 ou 3 ?"))
+classe = int(input("Qual será a sua classe escolhida 1 = barbaro,2 = mago ou 3 = arqueiro ?"))
 
-if (classe = 1 ):
+if (classe == 1 ):
     print("você escolheu a classe Barbaro ")
 
-elif (classe = 2):
+elif (classe == 2):
     print("voce escolheu a classe mago")
 
-elif (classe = 3):
+elif (classe == 3):
     print("voce escolheu a classe arqueiro")
 
-    
+else: 
+    print("comando não reconhecido pela escolha de classes")
     
     
     
