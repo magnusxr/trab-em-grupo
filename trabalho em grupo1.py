6d057fcdf728c533f6eecd623bef07d2f0e5443d
FCoppolla <felipe.v.coppolla@gmail.com>
2024-10-07 18:46:27 -0300

    part 1 do trabalho do curso
    fazendo a parte inicial

---
+++ trabalho em grupo1.py
@@ -0,0 +1,15 @@
+#faça um programa que se o usuario inserir 1 ele escolhe a classe barbaro, se 2 escolhe a classe mago, se 3 escolhe a classe arqueiro.
+
+
+int(input("Qual será a sua classe escolhida 1,2 ou 3 ?"))
+
+if (1 = barbaro):
+
+    elif (2 = mago)
+
+        elif (3 = arqueiro )
+    
+    
+    
+
+    
