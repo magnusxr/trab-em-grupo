e6a0cccea9e33a20c6fd7833996b08714080cb8d
FCoppolla <felipe.v.coppolla@gmail.com>
2024-10-07 18:51:40 -0300

    trabalho em grupo part 1
    primeira mudança da primeira ´parte

--- trabalho em grupo1.py
+++ trabalho em grupo1.py
@@ -1,13 +1,18 @@
 #faça um programa que se o usuario inserir 1 ele escolhe a classe barbaro, se 2 escolhe a classe mago, se 3 escolhe a classe arqueiro.
 
 
-int(input("Qual será a sua classe escolhida 1,2 ou 3 ?"))
+classe = int(input("Qual será a sua classe escolhida 1,2 ou 3 ?"))
 
-if (1 = barbaro):
+if (classe = 1 ):
+    print("você escolheu a classe Barbaro ")
 
-    elif (2 = mago)
+elif (classe = 2):
+    print("voce escolheu a classe mago")
 
-        elif (3 = arqueiro )
+elif (classe = 3):
+    print("voce escolheu a classe arqueiro")
+
+    
     
     
     
