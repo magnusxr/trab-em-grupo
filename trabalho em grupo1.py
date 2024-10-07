c4d2cb6a935f75cc6fccd0d4ed8a527f4d14b7d6
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-07 19:11:33 -0300

    atualização 2

--- trabalho em grupo1.py
+++ trabalho em grupo1.py
@@ -1,21 +1,42 @@
 #faça um programa que se o usuario inserir 1 ele escolhe a classe barbaro, se 2 escolhe a classe mago, se 3 escolhe a classe arqueiro.
 
 
-classe = int(input("Qual será a sua classe escolhida?\n 1 = barbaro\n 2 = mago\n 3 = arqueiro"))
+c = input("Qual será a sua classe escolhida?\n 1 = Bárbaro\n 2 = Mago\n 3 = Arqueiro\n > ")
 
-if (classe == 1 ):
-    print("você escolheu a classe Barbaro ")
+if (c == "1"):
+    classe = "Bárbaro"
 
-elif (classe == 2):
-    print("voce escolheu a classe mago")
+elif (c == "2"):
+    classe = "Mago"
 
-elif (classe == 3):
-    print("voce escolheu a classe arqueiro")
+elif (c == "3"):
+    classe = "Arqueiro"
 
 else: 
-    print("comando não reconhecido pela escolha de classes")
-    
-    
-    
+    print("Comando não reconhecido.")
 
-    
+print("Qual arma você gostaria de equipar?")
+print("1 - Adaga")
+print("2 - Espada Longa")
+print("3 - Arco e Flecha")
+print("4 - Cajado")
+print("")
+x = input("> ")
+
+if x == "1":
+    arma = "Adaga"
+
+elif x == "2":
+    arma = "Espada Longa"
+
+elif x == "3":
+    arma = "Arco e Flecha"
+
+elif x == "4":
+    arma = "Cajado"
+
+else:
+    print("Comando não reconhecido.")
+    print(x)
+
+print(f"Você é um {classe} com a arma {arma}!")
